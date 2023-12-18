2bbb174129c68e675f83adb7d6f03394d51d7c1c
Jo Young <githuborigin@gmail.com>
2023-12-18 11:08:16 +0900

    feat: 모달 배치

--- redux-study/src/App.jsx
+++ redux-study/src/App.jsx
@@ -1,7 +1,13 @@
+import OpenModalButton from "./components/OpenModalButton";
+import Modal from "./components/Modal/Modal";
 
 function App() {
-
-  return <div></div>;
+  return (
+    <div className="App">
+      <OpenModalButton />
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
