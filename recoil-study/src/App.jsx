342e28bc73bb38aae7d670fff4d573fb26b5bcb8
Jo Young <githuborigin@gmail.com>
2023-12-29 17:36:48 +0900

    feat:리코일 공식 문서 todoList 구현

--- recoil-study/src/App.jsx
+++ recoil-study/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
-import { TodoItem } from "./component/TodoItem";
-import { useRecoilState } from "recoil";
-import { todoListState } from "@/recoil/atom";
+import { useRecoilValue } from "recoil";
+import { filteredTodoListState } from "@/recoil/atom";
 import TodoItemCreator from "./component/TodoItemCreator";
+import TodoItem from "./component/TodoItem";
+import TodoListFilters from "@/component/TodoListFilters/index";
+import TodoListStats from "./component/todoListStates";
 
 function App() {
   // 리코일 스테이트
-  const todoList = useRecoilState(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
-      <p>asdkas</p>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
